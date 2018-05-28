9ad4502d379b8e6e80056a46018a1f7783e8f25f
Gurvir Cheema <gurvircheema@Gurvirs-iMac.hitronhub.home>
2018-05-28 10:13:13 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>React client for recipes backend</p>
+      </div>
+    );
+  }
+}
+
+export default App;
